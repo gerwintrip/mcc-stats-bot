cd843dbbb82f74d056ba18328d695364871e6432
Gerwin Trip <gerwintrip@gmail.com>
2023-06-18 15:56:59 +0200

    feat(datahandler): add event rundown to datahandler

--- src/handlers/datahandler.js
+++ src/handlers/datahandler.js
@@ -3,7 +3,13 @@ const { apiUrl } = require('../config.json');
 
 const eventEndpoint = `${apiUrl}/event`;
 const participantsEndpoint = (team) => team == '' ? `${apiUrl}/participants` : `${apiUrl}/participants/${team}`;
+const rundownEndpoint = (event) => event == '' ? `${apiUrl}/rundown` : `${apiUrl}/rundown/${event}`;
 
+/*
+ * Gets the event data of the current event cycle from the API.
+ * @returns {Object} The event data.
+ * @returns {null} If the API request failed.
+*/
 async function getEvent() {
 	const apiResponse = await axios.get(eventEndpoint);
 	if (apiResponse.status != 200) {
@@ -14,6 +20,12 @@ async function getEvent() {
 	return eventData;
 }
 
+/*
+ * Gets the participants data of the current event cycle from the API.
+ * @param {string} team The team to get the participants of.
+ * @returns {Object} The participants data.
+ * @returns {null} If the API request failed.
+*/
 async function getParticipants(team) {
 	const apiResponse = await axios.get(participantsEndpoint(team));
 	if (apiResponse.status != 200) {
@@ -24,8 +36,19 @@ async function getParticipants(team) {
 	return participantsData;
 }
 
+async function getRundown(event) {
+	const apiResponse = await axios.get(rundownEndpoint(event));
+	if (apiResponse.status != 200) {
+		console.error(`Error getting rundown: ${apiResponse.status} ${apiResponse.statusText}`);
+		return null;
+	}
+	const rundownData = apiResponse.data['data'];
+	return rundownData;
+}
+
 
 module.exports = {
 	getEvent,
 	getParticipants,
+	getRundown,
 };
